b3d3dcd83b31840b280e3f4d11dae6818aeaa72b
Jaehoon-So <jhspacelover@naver.com>
2023-12-06 23:08:18 +0900

    add: set project

---
+++ src/main/java/mangmae/harpseal/HarpsealApplication.java
@@ -0,0 +1,13 @@
+package mangmae.harpseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarpsealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarpsealApplication.class, args);
+	}
+
+}

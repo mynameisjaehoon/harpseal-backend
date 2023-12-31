9e3bb89cfe8a7b83d0c9e1cb6147f8313d5cc704
Jaehoon-So <jhspacelover@naver.com>
2024-01-01 06:19:14 +0900

    feat: password sah256 hash

--- src/main/java/mangmae/harpseal/global/util/SecurityUtil.java
+++ src/main/java/mangmae/harpseal/global/util/SecurityUtil.java
@@ -3,6 +3,9 @@ package mangmae.harpseal.global.util;
 import lombok.extern.slf4j.Slf4j;
 import mangmae.harpseal.global.exception.PasswordNotMatchException;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 @Slf4j
 public class SecurityUtil {
@@ -15,6 +18,28 @@ public class SecurityUtil {
         if (!password1.equals(password2)) {
             throw new PasswordNotMatchException("password is invalid");
         }
+    }
+
+    public static String encryptSha256(final String object) {
+
+        byte[] bytes = object.getBytes();
+        String result = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(bytes);
+
+            byte[] byteData = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xFF) + 256, 16).substring(1));
+            }
+
+            result = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
 }
